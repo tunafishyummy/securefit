30533895151cd1a0b3a4e0d59b4c8e2d0983dc22
tunafishyum <adinoellamio@gmail.com>
2026-04-06 22:13:02 +0800

    SwingUtilities.invokeLater, also experimenting with window sizing

--- Main.java
+++ Main.java
@@ -3,16 +3,21 @@ import java.awt.*;
 public class Main {
     public static JFrame window;
     public static void main(String[] args) {
-
+        
+        SwingUtilities.invokeLater(() -> { //I heard this does really good shit for stability
         window = new JFrame("Home");
-        //window.setSize(800, 600);
-        window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        window.setSize(800, 600);
         window.setUndecorated(false); //set to true for borderless
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         window.setLayout(null); //for manual placement
         window.getContentPane().setBackground(Color.WHITE);
-
-        window.setVisible(true);
+       
         HomePage.show();
+       
+        window.setVisible(true);
+
+        });
+        
     }
 }
